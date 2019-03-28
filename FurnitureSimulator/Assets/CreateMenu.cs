14a1d84240f5c0e11447671716f360301d7a8a77
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-03-28 18:35:32 -0500

    Creando nueva rama

---
+++ FurnitureSimulator/Assets/CreateMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0, 0.5f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
