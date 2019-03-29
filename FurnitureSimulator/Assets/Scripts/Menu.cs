11176479b6cf5d9720845a4ca8bce3668cb30a62
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-03-28 20:57:44 -0500

    Mi primer menu 3D interactivo

---
+++ FurnitureSimulator/Assets/Scripts/Menu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public int cantOpt;
+    public int r;
+    private GameObject menu;
+    public Material m1;
+    public Material m2;
+    public float distZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NextOption();
+    }
+
+    void NewMenu()
+    {
+        menu = GameObject.FindGameObjectWithTag("menu");
+        int anguloSep = 360 / cantOpt;
+        float angCubo = 90;
+        for (int i = 0; i < cantOpt; i++)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.tag = "opt" + i;
+            float x = r * Mathf.Cos(angCubo * Mathf.Deg2Rad);
+            float y = r * Mathf.Sin(angCubo * Mathf.Deg2Rad);
+            if (i == 0)
+            {
+                cube.transform.position = new Vector3(x, y, distZ);
+            }
+            else
+            {
+                cube.transform.position = new Vector3(x, y, 0);
+            }
+            cube.transform.localScale = new Vector3(2.0f, 1.0f, 0.01f);
+            cube.AddComponent<Renderer>();
+            if (i % 2 == 0)
+            {
+                cube.GetComponent<Renderer>().material = m1;
+            }
+            else
+            {
+                cube.GetComponent<Renderer>().material = m2;
+            }
+
+            cube.transform.parent = menu.transform;
+            angCubo += anguloSep;
+        }
+    }
+
+    void NextOption()
+    {
+
+    }
+}
---
+++ FurnitureSimulator/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    public float speed = 0.01f;
+    private GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = GameObject.FindGameObjectWithTag("menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        menu.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        //transform.Rotate(new Vector3(0.0f,yaw, pitch));
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            menu.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            menu.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);            
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+            menu.transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+            menu.transform.position -= new Vector3(0, 0, speed * Time.deltaTime);
+        }
+        Debug.Log(transform.position.ToString());
+    }
+}
