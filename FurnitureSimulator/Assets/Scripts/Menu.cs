1f22520b1f24a6d7fa6e9701911bd93cf35b8c19
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-04-02 00:53:04 -0500

    Adición Ver imagen y video en grande

--- FurnitureSimulator/Assets/Scripts/Menu.cs
+++ FurnitureSimulator/Assets/Scripts/Menu.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Menu : MonoBehaviour
 {
@@ -8,18 +11,20 @@ public class Menu : MonoBehaviour
     public float r;
     private GameObject menu;
     public Material m1;
-    public Material m2;
     public float distZ;
     private GameObject[] opts;
     private GameObject activeOpt;
     public float targetDistance;
     public LayerMask layerMask;
+    private GameObject centerObject;
+    string path;
     // Start is called before the first frame update
     void Start()
     {
         menu = GameObject.FindGameObjectWithTag("menu");
         CreateMenu();
-}
+        path = Application.dataPath;
+    }
 
     // Update is called once per frame
     void Update()
@@ -49,18 +54,31 @@ public class Menu : MonoBehaviour
         {
             menu.SetActive(false);
         }
-        if (Input.GetMouseButtonDown(0))
+        RaycastHit optHit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out optHit, targetDistance, layerMask))
         {
-            RaycastHit optHit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out optHit, targetDistance, layerMask))
+            if (Input.GetMouseButtonDown(0) && optHit.collider.tag.Equals("active") && !centerObject.activeSelf)
             {
-                if (optHit.collider.tag.Equals("active"))
+                centerObject.SetActive(true);
+                if (activeOpt.GetComponent<VideoPlayer>() != null)
                 {
-                    Debug.Log(optHit.collider.name);
+                    AddVideo(ref centerObject);
+                    centerObject.GetComponent<VideoPlayer>().isLooping = false;
+                    centerObject.GetComponent<VideoPlayer>().Pause();
+                }
+                else
+                {
+                    Destroy(centerObject.GetComponent<VideoPlayer>());
+                    centerObject.GetComponent<Renderer>().material = activeOpt.GetComponent<Renderer>().material;
                 }
             }
         }
+        if (Input.GetMouseButtonDown(1) && centerObject.activeSelf)
+        {
+            centerObject.SetActive(false);
+        }
+
     }
 
     void CreateMenu()
@@ -76,15 +94,24 @@ public class Menu : MonoBehaviour
             if (i == 0)
             {
                 cube.tag = "active";
-                cube.transform.position = new Vector3(x, y, distZ-(0.2f));
+                cube.transform.position = new Vector3(x, y, distZ - (0.1f));
             }
             else
             {
                 cube.tag = "not active";
                 cube.transform.position = new Vector3(x, y, distZ);
             }
-            cube.transform.localScale = new Vector3(0.1f, 0.05f, 0.01f);
-            cube.GetComponent<Renderer>().material.color = Random.ColorHSV();
+            if(i%2 == 0)
+            {
+                cube.GetComponent<Renderer>().material = m1;
+            }
+            else
+            {
+                AddVideo(ref cube);
+                cube.GetComponent<VideoPlayer>().SetDirectAudioMute(0,true);
+            }
+            cube.transform.localScale = new Vector3(0.2f, 0.1f, 0.01f);
+            cube.transform.localEulerAngles = new Vector3(0, 0, 180);
             cube.transform.parent = menu.transform;
             angCubo += anguloSep;
         }
@@ -93,6 +120,26 @@ public class Menu : MonoBehaviour
         {
             opts[i] = menu.transform.GetChild(i).gameObject;
         }
+        centerObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        centerObject.tag = "centerObject";
+        centerObject.transform.position = new Vector3(0f, 0f, -9.5f);
+        centerObject.transform.parent = menu.transform;
+        centerObject.transform.localScale = new Vector3(1.2f, 0.8f, 0.0f);
+        centerObject.SetActive(false);
+    }
+
+    void AddVideo (ref GameObject obj)
+    {
+        VideoPlayer video = obj.AddComponent<VideoPlayer>();
+        AudioSource audio = obj.AddComponent<AudioSource>();
+        video.clip = Resources.Load<VideoClip>("video") as VideoClip;
+        video.isLooping = true;
+        video.renderMode = VideoRenderMode.MaterialOverride;
+        video.targetMaterialRenderer = GetComponent<Renderer>();
+        video.targetMaterialProperty = "_MainTex";
+        video.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        video.SetTargetAudioSource(0, audio);
+        video.Play();
     }
 
     void RightOption(ref GameObject activeOpt)
@@ -112,7 +159,7 @@ public class Menu : MonoBehaviour
         {
             opts[0].tag = "active";
         }
-        opts[0].transform.position = new Vector3(x,y,z);
+        opts[0].transform.position = new Vector3(x, y, z);
         activeOpt.tag = "not active";
     }
 
