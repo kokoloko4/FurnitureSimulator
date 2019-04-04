f018503986e30dd64cc6dd90d8411a4da2ce95ae
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-04-03 23:08:56 -0500

    Improvements

--- FurnitureSimulator/Assets/Scripts/Menu.cs
+++ FurnitureSimulator/Assets/Scripts/Menu.cs
@@ -17,13 +17,11 @@ public class Menu : MonoBehaviour
     public float targetDistance;
     public LayerMask layerMask;
     private GameObject centerObject;
-    string path;
     // Start is called before the first frame update
     void Start()
     {
         menu = GameObject.FindGameObjectWithTag("menu");
         CreateMenu();
-        path = Application.dataPath;
     }
 
     // Update is called once per frame
@@ -67,10 +65,13 @@ public class Menu : MonoBehaviour
                     centerObject.GetComponent<VideoPlayer>().isLooping = false;
                     centerObject.GetComponent<VideoPlayer>().Pause();
                 }
-                else
+                else if(activeOpt.GetComponent<VideoPlayer>() == null)
                 {
                     Destroy(centerObject.GetComponent<VideoPlayer>());
                     centerObject.GetComponent<Renderer>().material = activeOpt.GetComponent<Renderer>().material;
+                    /*Scene sceneToLoad = SceneManager.GetSceneAt(1);
+                    SceneManager.LoadScene(sceneToLoad.name);
+                    SceneManager.MoveGameObjectToScene(centerObject, sceneToLoad);*/
                 }
             }
         }
@@ -107,8 +108,9 @@ public class Menu : MonoBehaviour
             }
             else
             {
-                AddVideo(ref cube);
-                cube.GetComponent<VideoPlayer>().SetDirectAudioMute(0,true);
+                //AddVideo(ref cube);
+                //cube.GetComponent<VideoPlayer>().SetDirectAudioMute(0,true);
+                AddModel(ref cube);
             }
             cube.transform.localScale = new Vector3(0.2f, 0.1f, 0.01f);
             cube.transform.localEulerAngles = new Vector3(0, 0, 180);
@@ -126,6 +128,16 @@ public class Menu : MonoBehaviour
         centerObject.transform.parent = menu.transform;
         centerObject.transform.localScale = new Vector3(1.2f, 0.8f, 0.0f);
         centerObject.SetActive(false);
+    }
+
+    void AddModel(ref GameObject parent)
+    {
+        parent.GetComponent<Renderer>().material = Resources.Load<Material>("ColorMenu2") as Material;
+        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        obj.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        obj.transform.parent = parent.transform;
+        obj.transform.localScale = new Vector3(parent.transform.localScale.x, parent.transform.localScale.y, parent.transform.localScale.z);
+        obj.transform.localEulerAngles = new Vector3(40f,0f,0f);
     }
 
     void AddVideo (ref GameObject obj)
