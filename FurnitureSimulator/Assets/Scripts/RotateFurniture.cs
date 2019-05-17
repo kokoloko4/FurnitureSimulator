6254736e98b803d16b3648cb7cd0173a10cb3164
Oscar Ivan Duarte Torres <o.duarte7@gmail.com>
2019-05-16 19:42:28 -0500

    Se agrega rotación

---
+++ FurnitureSimulator/Assets/Scripts/RotateFurniture.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateFurniture : MonoBehaviour
+{
+
+    private GameObject RightHand = null;
+    private GameObject LeftHand = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        RightHand = GetComponent<CollisionHands>().RightHand;
+        LeftHand = GetComponent<CollisionHands>().LeftHand;
+        if (RightHand != null && LeftHand != null && GetComponent<CollisionHands>().isGrabbed)
+        {
+
+       
+                if (Input.GetKey(KeyCode.O))
+                {
+                    transform.Rotate(0, 10, 0);
+                }
+                else if (Input.GetKey(KeyCode.L))
+                {
+                    transform.Rotate(0, -10, 0);
+                }
+                else if (Input.GetKey(KeyCode.P))
+                {
+                transform.Rotate(10, 0, 0);
+                }
+                else if (Input.GetKey(KeyCode.I))
+                {
+                transform.Rotate(-10, 0, 0);
+                }
+                else if (Input.GetKey(KeyCode.K))
+                {
+                transform.Rotate(0, 0, 10);
+                }
+                else if (Input.GetKey(KeyCode.M))
+                {
+                transform.Rotate(0, 0, -10);
+                }
+
+
+
+        }
+    }
+}
