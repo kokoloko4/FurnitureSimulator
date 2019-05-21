23a671a28f122e7d57ea91e920a86b988dd0d68f
kokoloko4 <anfecoquin@hotmail.com>
2019-05-21 11:15:30 -0500

    Set scenes

--- FurnitureSimulator/Assets/Scripts/Menus/BackgroundMovementMenu.cs
+++ FurnitureSimulator/Assets/Scripts/Menus/BackgroundMovementMenu.cs
@@ -13,6 +13,6 @@ public class BackgroundMovementMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.5f, 0);
+        transform.Rotate(0, 0.2f, 0);
     }
 }
