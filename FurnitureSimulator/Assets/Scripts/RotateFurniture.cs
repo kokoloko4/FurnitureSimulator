3ad0ccd32eed997ebbc6449f292c1dd6fcc1e7d3
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-05-20 19:33:26 -0500

    Fixed rotation

--- FurnitureSimulator/Assets/Scripts/RotateFurniture.cs
+++ FurnitureSimulator/Assets/Scripts/RotateFurniture.cs
@@ -7,81 +7,75 @@ using WiimoteApi;
 public class RotateFurniture : MonoBehaviour
 {
 
-    private GameObject RightHand = null;
-    private GameObject LeftHand = null;
+    private CollisionHands collision;
     public Wiimote controlWii = null;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        collision = GetComponent<CollisionHands>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        RightHand = GetComponent<CollisionHands>().RightHand;
-        LeftHand = GetComponent<CollisionHands>().LeftHand;
-
-        if (RightHand != null && LeftHand != null && GetComponent<CollisionHands>().isGrabbed)
+        controlWii = GetComponent<CollisionHands>().controlWii;
+        if ((collision.TouchingFurniture() && collision.isGrabbed && controlWii != null) || true)
         {
-                    controlWii = GetComponent<CollisionHands>().controlWii;
-
-
-            if (controlWii != null)
+            int ret;
+            do
             {
-                int ret;
-                do
+                ret = controlWii.ReadWiimoteData();
+
+                if (ret > 0 && controlWii.current_ext == ExtensionController.MOTIONPLUS)
                 {
-                    ret = controlWii.ReadWiimoteData();
+                    Vector3 offset = new Vector3(-controlWii.MotionPlus.PitchSpeed,
+                                                    controlWii.MotionPlus.YawSpeed,
+                                                    controlWii.MotionPlus.RollSpeed) / 95f; // Divide by 95Hz (average updates per second from wiimote)
+                                                                                            //wmpOffset += offset;
 
-                    if (ret > 0 && controlWii.current_ext == ExtensionController.MOTIONPLUS)
-                    {
-                        Vector3 offset = new Vector3(-controlWii.MotionPlus.PitchSpeed,
-                                                        controlWii.MotionPlus.YawSpeed,
-                                                        controlWii.MotionPlus.RollSpeed) / 95f; // Divide by 95Hz (average updates per second from wiimote)
-                                                                                                //wmpOffset += offset;
-
-                        //model.rot.Rotate(offset, Space.Self);
-                    }
-                } while (ret > 0);
-
-                NunchuckData data = controlWii.Nunchuck;
-
-                Debug.Log("Stick: " + data.stick[0] + ", " + data.stick[1]);
-
-
-                if (data.stick[0] - 140 > 10 && data.c && !data.z)
+                    //model.rot.Rotate(offset, Space.Self);
+                }
+            } while (ret > 0);
+            NunchuckData data = controlWii.Nunchuck;
+            Debug.Log("Stick: " + data.stick[0] + ", " + data.stick[1]);
+            if (data.c && !data.z)
+            {
+                transform.parent = GameObject.FindGameObjectWithTag("vista").transform;
+                transform.position = new Vector3(transform.parent.position.x, transform.parent.position.y, transform.parent.position.z);
+                transform.localScale = collision.actualScale / 3;
+                if (data.stick[0] - 140 > 10)
                 {
                     transform.Rotate(0, 10, 0);
                 }
-                else if (data.stick[0] - 140 < -10 && data.c && !data.z)
+                else if (data.stick[0] - 140 < -10)
                 {
                     transform.Rotate(0, -10, 0);
 
                 }
-                else if (data.stick[1] - 130 > 10 && data.c && !data.z)
+                else if (data.stick[1] - 130 > 10)
                 {
                     transform.Rotate(10, 0, 0);
                 }
-                else if (data.stick[1] - 130 < -10 && data.c && !data.z)
+                else if (data.stick[1] - 130 < -10)
                 {
                     transform.Rotate(-10, 0, 0);
                 }
-                else if (data.stick[1] - 130 > 10 && data.c && data.z)
+                else if (data.stick[1] - 130 > 10)
                 {
                     transform.Rotate(0, 0, 10);
                 }
-                else if (data.stick[1] - 130 < -10 && data.c && data.z)
+                else if (data.stick[1] - 130 < -10)
                 {
                     transform.Rotate(0, 0, -10);
                 }
-
             }
-
-
+            else
+            {
+                transform.parent = collision.RightHand.gameObject.transform;
+                transform.position = new Vector3(transform.parent.position.x, transform.parent.position.y, transform.parent.position.z);
+                transform.localScale = collision.actualScale;
+            }
         }
     }
 }
